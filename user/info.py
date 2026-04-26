ab47f257fc192a976694ca9e46e6da1ecebe458d
|GriB28| <gp.off.dev@gmail.com>
2026-04-26 20:00:41 +0300

    [b-16] получение статы юзера по логину

--- user/info.py
+++ user/info.py
@@ -72,6 +72,38 @@ async def get_by_email(email: str) -> dict[str, ...]:
             return json
 
 
+async def get_by_login(login: str) -> dict[str, ...]:
+    """
+    Запрос данных о пользователе в следующем формате:
+
+    | {
+    | "ID": int,
+    | "name": str,
+    | "login": str | None,
+    | "password": str | None,
+    | "group": str,
+    | "email": str,
+    | "tag": str | None,
+    | "balance": int,
+    | "owner": int
+    | }
+
+    :param login: login пользователя
+    :return: словарь с данными пользователя из таблицы ``database.USERS``
+    """
+
+    async with ClientSession(connector=TCPConnector(ssl=CONFIGURATION.SSL)) as session:
+        async with session.get(
+                f"{CONFIGURATION.HOST}:{CONFIGURATION.PORT}/user/get",
+                params={"login": login}
+        ) as response:
+            json = await response.json()
+            if response.status >= 400:
+                raise APIError.get(get_by_login, response.status, json)
+
+            return json
+
+
 async def get_all() -> list[int]:
     """
     Запрос всех существующих ID пользователей
