76150c13ee58561b63a4f864520b96615e2f09de
|GriB28| <gp.off.dev@gmail.com>
2026-05-07 00:04:13 +0300

    [b-20] в конфигурацию добавлена выгрузка токена

--- __config__.py
+++ __config__.py
@@ -13,6 +13,7 @@ class CONFIGURATION:
 
     HOST = getenv("LYPAY_CORE_HOST")
     PORT = int(getenv("LYPAY_CORE_PORT"))
+    TOKEN = getenv("LYPAY_CORE_TOKEN")
 
     SSL = ssl_create_default_context()
     SSL.check_hostname = False
